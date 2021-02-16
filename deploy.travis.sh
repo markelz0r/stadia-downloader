75fac488867829df13a56110643d07ae4316abbb
Andrew Odintsov <a1covker@gmail.com>
2021-02-16 23:26:31 +0000

    change deployment

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -4,5 +4,6 @@ if [ "$TRAVIS_OS_NAME" == osx ]; then
     yarn release
 else
     # deploy on windows and linux
-    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux deb --win"
+    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux deb"
+    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "rm -rf node_modules && yarn --link-duplicates --pure-lockfile && yarn release --linux deb --win"
 fi
